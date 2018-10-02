bc0b4a9113ff524477a615027d4bc48f1b641d9d
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-10-02 21:22:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,3 +8,15 @@ def line(num_in_line)
     puts "The line is currently: " + new_array.join(" ")
   end
 end
+
+# 2 Build a method that a new customer will use when entering the deli. 
+# The `take_a_number` method should accept two arguments, the array for 
+# the current line of people (`katz_deli`), and a string containing the 
+# name of the person wishing to join the line. 
+# The method should return the person's name along with their position in line. 
+#**Top-Tip:** *Remember that people like to count from* `1`*, not from* `0` *("zero") 
+#like computers.*
+
+def take_a_number(katz_deli, new_customer)
+  katz_deli.push(new_customer
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line.")
