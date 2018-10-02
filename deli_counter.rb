85586a6a72e125208a72e1ef94e3e3e972103216
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-10-02 21:22:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,12 +9,12 @@ def line(num_in_line)
   end
 end
 
-# 2 Build a method that a new customer will use when entering the deli. 
-# The `take_a_number` method should accept two arguments, the array for 
-# the current line of people (`katz_deli`), and a string containing the 
-# name of the person wishing to join the line. 
-# The method should return the person's name along with their position in line. 
-#**Top-Tip:** *Remember that people like to count from* `1`*, not from* `0` *("zero") 
+# 2 Build a method that a new customer will use when entering the deli.
+# The `take_a_number` method should accept two arguments, the array for
+# the current line of people (`katz_deli`), and a string containing the
+# name of the person wishing to join the line.
+# The method should return the person's name along with their position in line.
+#**Top-Tip:** *Remember that people like to count from* `1`*, not from* `0` *("zero")
 #like computers.*
 
 def take_a_number(katz_deli, new_customer)
